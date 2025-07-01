981f45179616661383bd768a1593ee372ab6c0fb
byteshake-habib <habib@themepaste.com>
2025-07-01 14:53:06 +0600

    misc

--- views/settings/fields/text.php
+++ views/settings/fields/text.php
@@ -1,21 +1,28 @@
-<?php 
+<?php
+    /**
+     * Output a text input field.
+     *
+     * @package ThemePaste
+     */
+
     defined( 'ABSPATH' ) || exit;
  
-    printf( '<div class="tp-field">
-                <div class="tp-field-label">
-                    <label>%1$s</label>
+    printf( '
+        <div class="tp-field">
+            <div class="tp-field-label">
+                <label>%1$s</label>
+            </div>
+            <div class="tp-field-input">
+                <div class="tp-switch-wrapper">
+                    <input type="text" id="%2$s" name="%2$s" value="%3$s">
                 </div>
-                <div class="tp-field-input">
-                    <div class="tp-switch-wrapper">
-                        <input type="text" id="%2$s" name="%2$s" value="%3$s">
-                    </div>
-                    <p class="tp-field-desc">%4$s</p>
-                </div>
-            </div>',
-            $args['value']['label'],                                                    // %1$s == Label
-            $args['prefix'] . '-' . $args['current_screen_slug'] . '_' . $args['key'],  // %2$s == ID & Name
-            $args['value']['id'],                                                       // %3$s == value
-            $args['value']['desc']                                                      // %4$s == Description
+                <p class="tp-field-desc">%4$s</p>
+            </div>
+        </div>',
+        $args['value']['label'],                                                    // %1$s == Label
+        $args['prefix'] . '-' . $args['current_screen_slug'] . '_' . $args['key'],  // %2$s == ID & Name
+        $args['value']['id'],                                                       // %3$s == value
+        $args['value']['desc']                                                      // %4$s == Description
     );
 ?>
 
