8cb5d254aa53633a490924cf9367c4aedead4740
byteshake-habib <habib@themepaste.com>
2025-07-03 18:05:09 +0600

    tab design added

--- spa/admin/login-logs-activity/Main.jsx
+++ spa/admin/login-logs-activity/Main.jsx
@@ -6,7 +6,7 @@ import FailedLogins from './components/FailedLogins';
 import SuccessfulLogins from './components/SuccessfulLogins';
 
 function Main() {
-    const [activeComponent, setActiveComponent] = useState('SuccessfulLogins');
+    const [activeComponent, setActiveComponent] = useState('BlockUsers');
 
     const renderComponent = () => {
         switch (activeComponent) {
@@ -26,25 +26,38 @@ function Main() {
             <div className="tpsa-login-log-activity-wrapper">
                 <div className="tpsa-login-log-activity-tabs">
                     <button
+                        className={
+                            activeComponent === 'BlockUsers' ? 'active' : ''
+                        }
                         type="button"
                         onClick={() => setActiveComponent('BlockUsers')}
                     >
                         Block Users
                     </button>
                     <button
+                        className={
+                            activeComponent === 'FailedLogins' ? 'active' : ''
+                        }
                         type="button"
                         onClick={() => setActiveComponent('FailedLogins')}
                     >
                         Failed Logins
                     </button>
                     <button
+                        className={
+                            activeComponent === 'SuccessfulLogins'
+                                ? 'active'
+                                : ''
+                        }
                         type="button"
                         onClick={() => setActiveComponent('SuccessfulLogins')}
                     >
                         Successful Logins
                     </button>
                 </div>
-                <div>{renderComponent()}</div>
+                <div className="tpsa-login-log-activity-container">
+                    {renderComponent()}
+                </div>
             </div>
         </>
     );
