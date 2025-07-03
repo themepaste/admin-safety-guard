7b5ea53561f12159f0be387d7b9e23be988504cf
byteshake-habib <habib@themepaste.com>
2025-07-03 13:07:40 +0600

    textarea field added

---
+++ views/settings/fields/textarea.php
@@ -0,0 +1,32 @@
+<?php
+    /**
+     * Output a text input field.
+     *
+     * @package ThemePaste
+     */
+
+    defined( 'ABSPATH' ) || exit;
+
+    $id_name    = esc_attr( $args['prefix'] . $args['current_screen_slug'] . '_' . $args['key'] );
+    $value      = isset( $args['value'] ) && ! empty( $args['value'] ) ? $args['value'] : '';
+    
+    $field_template = '
+        <div class="tp-field">
+            <div class="tp-field-label">
+                <label>%1$s</label>
+            </div>
+            <div class="tp-field-input">
+                <div class="tp-switch-wrapper">
+                    <textarea name="%2$s" id="%2$s">%3$s</textarea>
+                </div>
+                <p class="tp-field-desc">%4$s</p>
+            </div>
+        </div>';
+
+    printf( $field_template,
+        esc_html( $args['field']['label'] ),    // %1$s == Label
+        $id_name,                               // %2$s == ID & Name
+        esc_attr( $value ),                     // %3$s == value
+        esc_html( $args['field']['desc'] )      // %4$s == Description
+    );
+?>
