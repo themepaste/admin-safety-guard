c9ee66dc019ef7bff2132887437517dafdb3f654
byteshake-habib <habib@themepaste.com>
2025-07-03 14:36:05 +0600

    webpack config file added

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = (env, argv) => {
+    return {
+        mode: argv.mode || 'development',
+        entry: {
+            lockouts: path.resolve(__dirname, './spa/admin/'),
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, './assets/admin/build'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-react',
+                                '@babel/preset-env',
+                            ],
+                        },
+                    },
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.js', '.jsx'],
+        },
+    };
+};
