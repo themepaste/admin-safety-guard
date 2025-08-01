3f45f1f3b6c399065af82dd4b47179b2ca5d2ad9
byteshake-habib <habib@themepaste.com>
2025-08-01 16:22:51 +0600

    misc

--- views/settings/pages/two-factor-auth.php
+++ views/settings/pages/two-factor-auth.php
@@ -15,7 +15,14 @@
 
 <div class="tpsa-setting-wrapper">
     <div class="tpsa-general-settings-wrapper">
-        <h2><?php echo esc_html( 'Two Factor Authentication Settings' ); // page_label; ?></h2>
+        <h2><?php echo esc_html( 'Two Factor Authentication Settings' ); // page_label; ?>
+            <div class="tp-feature">
+                <button class="tp-help-icon">?</button>
+                <div class="tp-tooltip">
+                    <p>This feature adds a critical second layer of login protection, requiring a time-sensitive code from the user’s personal device.</p>
+                </div>
+            </div>
+        </h2>
         <form method="POST" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>">
             <?php wp_nonce_field( 'tpsa-nonce_action', 'tpsa-nonce_name' ); ?>
             <input type="hidden" name="action" value="tpsa_process_form">
