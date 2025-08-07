61d11fd48836b5492117cefa5b62243e945695ea
byteshake-habib <habib@themepaste.com>
2025-08-07 14:12:48 +0600

    setup widzard done

--- views/wizard/wizard.php
+++ views/wizard/wizard.php
@@ -15,11 +15,11 @@ defined( 'ABSPATH' ) || exit;
         <form action="" method="post">
             <?php wp_nonce_field( 'tpsm-nonce_action', 'tpsm-nonce_name' ); ?>
             <input type="hidden" name="tpsm_optin_submit" value="1">
-            <button type="submit" name="tpsm_optin_choice" value="0" class="button button-secondary">
+            <button type="submit" name="tpsm_optin_choice" value="0" class="button button-secondary tpsm-optin-deny">
                 <?php esc_html_e( 'Not now', 'shipping-manager' ); ?>
             </button>
             
-            <button type="submit" name="tpsm_optin_choice" value="1" class="active button button-primary">
+            <button type="submit" name="tpsm_optin_choice" value="1" class="active button button-primary tpsm-optin-allow">
                 <?php esc_html_e( 'Allow & Continue', 'shipping-manager' ); ?>
             </button>
         </form>
