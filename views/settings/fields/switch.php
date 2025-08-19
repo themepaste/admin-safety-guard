6111e83c5b5a33ae495a48dd5d4e4806491c548d
byteshake-habib <habib@themepaste.com>
2025-08-19 13:51:44 +0600

    switch filed modifyed added filter hook support

--- views/settings/fields/switch.php
+++ views/settings/fields/switch.php
@@ -1,30 +1,33 @@
 <?php 
+/**
+ * Output a switch / checkbox input field with filter hook support.
+ *
+ * @package ThemePaste
+ */
+defined( 'ABSPATH' ) || exit;
 
-     /**
-     * Output a switch / checkbox input field.
-     *
-     * @package ThemePaste
-     */
-    defined( 'ABSPATH' ) || exit;
+$id_name = esc_attr( $args['prefix'] . $args['current_screen_slug'] . '_' . $args['key'] );
+$value   = isset( $args['value'] ) && ! empty( $args['value'] ) ? 'checked' : '';
 
-    $value = isset( $args['value'] ) && ! empty( $args['value'] ) ? 'checked' : '';
-
-    printf( '
-        <div class="tp-field">
-            <div class="tp-field-label">
-                <label>%1$s</label>
+$field_template = '
+    <div class="tp-field">
+        <div class="tp-field-label">
+            <label>%1$s</label>
+        </div>
+        <div class="tp-field-input">
+            <div class="tp-switch-wrapper">
+                <input class="tp-switch" type="checkbox" id="%2$s" name="%2$s" %3$s />
+                <label for="%2$s" class="tp-switch-label"></label>
             </div>
-            <div class="tp-field-input">
-                <div class="tp-switch-wrapper">
-                    <input class="tp-switch" type="checkbox" id="%2$s" name="%2$s" %3$s /><label for="%2$s" class="tp-switch-label"></label>
-                </div>
-                <p class="tp-field-desc">%4$s</p>
-            </div>
-        </div>',
-        esc_html( $args['field']['label'] ),                                                    // %1$s == Label
-        esc_attr( $args['prefix'] . $args['current_screen_slug'] . '_' . $args['key'] ),        // %2$s == ID & Name
-        esc_attr( $value ),                                                                     // %3$s == value
-        esc_html( $args['field']['desc'] )                                                      // %4$s == Description
-    );
-?>
+            <p class="tp-field-desc">%4$s</p>
+        </div>
+    </div>';
+$field_template = apply_filters( $id_name, $field_template, $args );
 
+printf(
+    $field_template,
+    esc_html( $args['field']['label'] ), // %1$s == Label
+    $id_name,                            // %2$s == ID & Name
+    esc_attr( $value ),                  // %3$s == Checked
+    esc_html( $args['field']['desc'] )   // %4$s == Description
+);
