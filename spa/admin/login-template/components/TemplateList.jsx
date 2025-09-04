25d4978a1e7478796ecfdd5fd6c3c3395819dc67
byteshake-habib <habib@themepaste.com>
2025-09-04 14:28:23 +0600

    misc

--- spa/admin/login-template/components/TemplateList.jsx
+++ spa/admin/login-template/components/TemplateList.jsx
@@ -23,20 +23,35 @@ const TemplateList = () => {
             screenshotSmall: smalImg,
             screenshot: bigImg,
         },
+        {
+            id: 4,
+            name: 'Template 4',
+            screenshotSmall: smalImg,
+            screenshot: bigImg,
+        },
     ];
 
     return (
         <div className="template-list">
             {templateList.map((template) => (
                 <div className="single-template" key={template.id}>
-                    <h3>{template.name}</h3>
                     <ModalImage
+                        className="template-screenshot"
                         small={template.screenshotSmall}
                         large={template.screenshot}
                         alt={template.name}
                         hideDownload={true} // show download button
                         hideZoom={true}
                     />
+                    <div className="template-content">
+                        <h3>{template.name}</h3>
+                        <button
+                            type="button"
+                            className="button-primary use-template"
+                        >
+                            Use Template
+                        </button>
+                    </div>
                 </div>
             ))}
         </div>
