190d7b63cc8b57585151c76d0fa82eb6b9b25f2a
byteshake-habib <habib@themepaste.com>
2025-10-01 14:10:49 +0600

    misc

--- admin-safety-guard.php
+++ admin-safety-guard.php
@@ -2,23 +2,23 @@
 /*
 Plugin Name: Admin Safety Guard
 Plugin URI: http://themepaste.com/product/themepaste-secure-admin-pro/
-Description: Secure your WordPress login with Admin safety guard to ensure secured access with limit login attempts, 2FA, reCaptcha, IP Blocking, Disable XML-RPC and activity tracking.  
+Description: Secure your WordPress login with Admin safety guard to ensure secured access with limit login attempts, 2FA, reCaptcha, IP Blocking, Disable XML-RPC and activity tracking.
 Version: 1.0.4
 Author: Themepaste Team
 Author URI: http://themepaste.com/
 License: GPL2 or Later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: tp-secure-plugin
-*/
+ */
 
 if ( !defined( 'ABSPATH' ) ) {
     exit;
 } // Exit if accessed directly
 
- /**
-  * Plugin Main Class
-  */
-  final class TPSucureAdmin {
+/**
+ * Plugin Main Class
+ */
+final class TPSucureAdmin {
     static $instance = false;
 
     /**
@@ -34,24 +34,23 @@ if ( !defined( 'ABSPATH' ) ) {
      * define all constant
      */
     private function define() {
-       define( "TPSA_DEVS", true ); // 'true' | is development mode on
+        define( "TPSA_DEVS", true ); // 'true' | is development mode on
 
-       define( 'TPSA_PLUGIN_FILE', __FILE__ );
-       define( 'TPSA_PREFIX', 'tpsa' );
-       define( 'TPSA_PLUGIN_VERSION', '1.0.4' );
-       define( 'TPSA_PLUGIN_DIRNAME', dirname( TPSA_PLUGIN_FILE ) );
-       define( 'TPSA_PLUGIN_BASENAME', plugin_basename( TPSA_PLUGIN_FILE ) );
-       define( 'TPSA_PLUGIN_DIR', plugin_dir_path( TPSA_PLUGIN_FILE ) );
-       define( 'TPSA_PLUGIN_URL', plugin_dir_url( TPSA_PLUGIN_FILE ) );
-       define( 'TPSA_ASSETS_URL', plugins_url( 'assets', TPSA_PLUGIN_FILE ) );
-       define( 'TPSA_REAL_PATH', realpath( dirname( TPSA_PLUGIN_DIR ) ) );
+        define( 'TPSA_PLUGIN_FILE', __FILE__ );
+        define( 'TPSA_PREFIX', 'tpsa' );
+        define( 'TPSA_PLUGIN_VERSION', '1.0.4' );
+        define( 'TPSA_PLUGIN_DIRNAME', dirname( TPSA_PLUGIN_FILE ) );
+        define( 'TPSA_PLUGIN_BASENAME', plugin_basename( TPSA_PLUGIN_FILE ) );
+        define( 'TPSA_PLUGIN_DIR', plugin_dir_path( TPSA_PLUGIN_FILE ) );
+        define( 'TPSA_PLUGIN_URL', plugin_dir_url( TPSA_PLUGIN_FILE ) );
+        define( 'TPSA_ASSETS_URL', plugins_url( 'assets', TPSA_PLUGIN_FILE ) );
+        define( 'TPSA_REAL_PATH', realpath( dirname( TPSA_PLUGIN_DIR ) ) );
 
-       if( TPSA_DEVS ) {
-           define( 'TPSA_ASSETS_VERSION', time() );
-       }
-       else {
-           define( 'TPSA_ASSETS_VERSION', TPSA_PLUGIN_VERSION );
-       }
+        if ( TPSA_DEVS ) {
+            define( 'TPSA_ASSETS_VERSION', time() );
+        } else {
+            define( 'TPSA_ASSETS_VERSION', TPSA_PLUGIN_VERSION );
+        }
     }
 
     /**
@@ -63,7 +62,7 @@ if ( !defined( 'ABSPATH' ) ) {
 
         /**
          * Check if the Composer autoloader class for TPShippingManager exists.
-         * 
+         *
          * The class name usually includes the suffix defined in the composer.json
          * file, typically something like 'ComposerAutoloaderInitTPShippingManager'.
          *
@@ -71,16 +70,16 @@ if ( !defined( 'ABSPATH' ) ) {
          * register the necessary autoload mappings.
          */
         // if ( ! class_exists( 'ComposerAutoloaderInitTPShippingManager' ) ) {
-		require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+        require_once dirname( __FILE__ ) . '/vendor/autoload.php';
         // }
     }
 
     /**
      * Singleton Instance
-    */
+     */
     static function get_instance() {
-        
-        if( ! self::$instance ) {
+
+        if ( !self::$instance ) {
             self::$instance = new self();
         }
 
