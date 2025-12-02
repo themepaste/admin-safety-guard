a23e82b634d51a5336ed2d970eb882502c65df6f
byteshake-habib <habib@themepaste.com>
2025-12-02 18:55:05 +0600

    poup added

---
+++ views/popup/pro-features-popup.php
@@ -0,0 +1,31 @@
+<div class="tpsa-pro-popup-overlay" id="tpsaProPopup">
+    <div class="tpsa-pro-popup-box">
+        <div class="tpsa-pro-popup-header">
+            <h2>Premium Feature</h2>
+            <button class="tpsa-pro-close">&times;</button>
+        </div>
+
+        <div class="tpsa-pro-popup-body">
+            <p>This feature is available only in the <strong>Pro version</strong>.
+                Please upgrade to unlock all premium protections and advanced features.</p>
+
+            <div class="tpsa-pro-icon-box">
+                <img src="https://cdn-icons-png.flaticon.com/512/1828/1828640.png" alt="Pro Icon">
+            </div>
+
+            <div class="tpsa-pro-buttons">
+                <a href="https://themepaste.com/" target="_blank" class="tpsa-pro-btn purchase-btn">
+                    Purchase Pro
+                </a>
+
+                <a href="#" id="openLicenseBox" class="tpsa-pro-btn license-btn">
+                    Activate License
+                </a>
+            </div>
+
+            <div class="tpsa-pro-small-link">
+                <a href="#" id="alreadyPurchased">Already Purchased?</a>
+            </div>
+        </div>
+    </div>
+</div>
