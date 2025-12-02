d1adc3ef736754e3527f76b7eaacd2e42a6e815b
byteshake-habib <habib@themepaste.com>
2025-12-02 18:41:42 +0600

    added the popup

--- views/settings/pages/web-application-firewall.php
+++ views/settings/pages/web-application-firewall.php
@@ -11,11 +11,12 @@ $submit_button = $prefix . '-' . $screen_slug . '_submit';
 $option_name = $args['option_name'];
 $saved_settings = get_option( $option_name, [] );
 $current_settings_fields = $args['settings_fields'][$screen_slug]['fields'] ?? [];
+$is_pro = true;
 ?>
 
 <div class="tpsa-setting-wrapper">
     <div class="tpsa-general-settings-wrapper">
-        <h2><?php echo esc_html( $page_label . ' ' . 'Settings' ); // page_label;    ?>
+        <h2><?php echo esc_html( $page_label . ' ' . 'Settings' ); // page_label;        ?>
             <div class="tp-feature">
                 <button class="tp-help-icon">?</button>
                 <div class="tp-tooltip">
@@ -56,5 +57,39 @@ printf( '<button type="submit">%1$s</button>',
 ?>
             </div>
         </form>
+    </div>
+</div>
+
+
+<!-- PRO POPUP OVERLAY -->
+<div class="tpsa-pro-popup-overlay" id="tpsaProPopup">
+    <div class="tpsa-pro-popup-box">
+        <div class="tpsa-pro-popup-header">
+            <h2>Premium Feature</h2>
+            <button class="tpsa-pro-close">&times;</button>
+        </div>
+
+        <div class="tpsa-pro-popup-body">
+            <p>This feature is available only in the <strong>Pro version</strong>.
+                Please upgrade to unlock all premium protections and advanced features.</p>
+
+            <div class="tpsa-pro-icon-box">
+                <img src="https://cdn-icons-png.flaticon.com/512/1828/1828640.png" alt="Pro Icon">
+            </div>
+
+            <div class="tpsa-pro-buttons">
+                <a href="https://themepaste.com/" target="_blank" class="tpsa-pro-btn purchase-btn">
+                    Purchase Pro
+                </a>
+
+                <a href="#" id="openLicenseBox" class="tpsa-pro-btn license-btn">
+                    Activate License
+                </a>
+            </div>
+
+            <div class="tpsa-pro-small-link">
+                <a href="#" id="alreadyPurchased">Already Purchased?</a>
+            </div>
+        </div>
     </div>
 </div>
