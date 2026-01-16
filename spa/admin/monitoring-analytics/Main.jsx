53c133189a6db8f87124c389420bf2a5726675ec
byteshake-habib <habib@themepaste.com>
2026-01-16 18:13:37 +0600

    monitoring analytics

--- spa/admin/monitoring-analytics/Main.jsx
+++ spa/admin/monitoring-analytics/Main.jsx
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client';
 function Main() {
   return (
     <>
-      <h1>Firewall Malware</h1>
+      <h1>Monitoring Analytics</h1>
     </>
   );
 }
 
 ReactDOM.createRoot(
-  document.getElementById('tpsa-firewall-malware-wrapper')
+  document.getElementById('tpsa-monitoring-analytics-wrapper')
 ).render(<Main />);
 
 export default Main;
