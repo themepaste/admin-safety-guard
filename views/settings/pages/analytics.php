f06604f7dee86bdac4176336d4d888a417e2d4a4
byteshake-habib <habib@themepaste.com>
2026-01-16 16:08:33 +0600

    dashboard fixed

--- views/settings/pages/analytics.php
+++ views/settings/pages/analytics.php
@@ -1,22 +1,13 @@
-<?php 
-    defined( 'ABSPATH' ) || exit;
-    
-    use ThemePaste\SecureAdmin\Helpers\Utility; 
+<?php
+defined( 'ABSPATH' ) || exit;
 
-    $prefix          = $args['prefix'];
-    $screen_slug     = $args['current_screen'];
-    $settings_option = $args['settings_option'];
-    $page_label      = $args['page_label'];
-    $submit_button   = $prefix . '-' . $screen_slug . '_submit';
-    $option_name     = $args['option_name'];
-    $saved_settings  = get_option( $option_name, [] );
-    $current_settings_fields = $args['settings_fields'][$screen_slug]['fields'] ?? [];
+$page_label = $args['page_label'];
 ?>
 
 <div class="tpsa-setting-wrapper">
     <div class="tpsa-general-settings-wrapper">
-        <h2><?php echo esc_html( $page_label ); // page_label; ?></h2>
-        
+        <h2><?php echo esc_html( $page_label ); // page_label;     ?></h2>
+
         <div class="tpsa-setting-row">
             <div id="tpsa-analytics-wrapper"></div>
         </div>
