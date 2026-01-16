ea7d11d24961034c8b1f8fdf37a5633a6e184868
byteshake-habib <habib@themepaste.com>
2026-01-16 16:33:29 +0600

    misc

--- views/settings/pages/customization.php
+++ views/settings/pages/customization.php
@@ -6,7 +6,7 @@ $page_label = $args['page_label'];
 
 <div class="tpsa-setting-wrapper">
     <div class="tpsa-general-settings-wrapper">
-        <h2><?php echo esc_html( $page_label ); // page_label;          ?></h2>
+        <h2><?php echo esc_html( $page_label ); // page_label;           ?></h2>
 
         <div class="tpsa-setting-row">
             <div id="tpsa-security-core-wrapper"></div>
