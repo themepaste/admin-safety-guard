8fa5149f7d31cc2332d8063810e4fe1b8db6fbd7
byteshake-habib <habib@themepaste.com>
2026-01-20 15:47:32 +0600

    misc

--- spa/admin/security-core/components/FeatureGrid.jsx
+++ spa/admin/security-core/components/FeatureGrid.jsx
@@ -141,7 +141,7 @@ export default function FeatureGrid({
                     Configure Settings
                   </button>
 
-                  <button
+                  {/* <button
                     type="button"
                     className="fg-secondaryBtn"
                     onClick={(e) => {
@@ -151,7 +151,7 @@ export default function FeatureGrid({
                     }}
                   >
                     View Details
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </div>
