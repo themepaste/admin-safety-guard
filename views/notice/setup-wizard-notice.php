5f49da55b4415d262e3ae76405bf901487dea46a
byteshake-habib <habib@themepaste.com>
2026-02-03 13:13:43 +0600

    misc

--- views/notice/setup-wizard-notice.php
+++ views/notice/setup-wizard-notice.php
@@ -2,17 +2,19 @@
 defined( 'ABSPATH' ) || exit;
 $setup_url = esc_url( admin_url( 'admin.php?page=tpasg_setup_wizard' ) );
 ?>
-    <div class="notice notice-warning is-dismissible tpsm-setup-notice">
-        <p style="display: flex; align-items: center; justify-content: space-between;">
-            <span>
-                <strong>
-                    <?php esc_html_e( '🎉 Welcome!', 'admin-safety-guard' )?>
-                </strong>
-                <?php esc_html_e( 'Before you can use' ); ?> <strong><?php esc_html_e( 'Admin Safety Guard,', 'admin-safety-guard' )?></strong>
-                <?php echo esc_html( 'Please complete the setup wizard.', 'admin-safety-guard' ); ?>
+<div class="notice notice-warning is-dismissible tpsm-setup-notice">
+    <p style="display: flex; align-items: center; justify-content: space-between;">
+        <span>
+            <strong>
+                <?php esc_html_e( '🎉 Welcome!', 'admin-safety-guard' )?>
+            </strong>
+            <?php esc_html_e( 'Before you can use', 'admin-safety-guard' ); ?>
+            <strong><?php esc_html_e( 'Admin Safety Guard,', 'admin-safety-guard' )?></strong>
+            <?php echo esc_html( 'Please complete the setup wizard.', 'admin-safety-guard' ); ?>
 
-            </span>
-            <!-- <br> -->
-            <a href="<?php echo $setup_url; ?>" class="button button-primary tpasg-notice-button"><?php esc_html_e( 'Launch Setup Wizard', 'admin-safety-guard' )?></a>
-        </p>
-    </div>
+        </span>
+        <!-- <br> -->
+        <a href="<?php echo $setup_url; ?>"
+            class="button button-primary tpasg-notice-button"><?php esc_html_e( 'Launch Setup Wizard', 'admin-safety-guard' )?></a>
+    </p>
+</div>
