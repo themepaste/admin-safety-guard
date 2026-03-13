c52837eb049d29e6d4607b2fb2494298533ad12d
byteshake-habib <habib@themepaste.com>
2026-03-13 12:35:34 +0600

    all layout added

--- views/settings/parts/main.php
+++ views/settings/parts/main.php
@@ -9,6 +9,7 @@ use ThemePaste\SecureAdmin\Helpers\Utility;
 <!-- Main content wrapper for the Shipping Manager settings page -->
 <div class="tpsa-main-wrapper">
     <?php
+
 // Loop through the available settings options
 foreach ( $settings_option as $key => $value ) {
 
@@ -25,27 +26,11 @@ foreach ( $settings_option as $key => $value ) {
                 }
             }
         }
-
     }
 
     // Check if the current screen matches the key (active tab)
     if ( $current_screen === $key ) {
-
-        // Attempt to retrieve the template for the active settings page
-        {
-            $template = Utility::get_template( 'settings/pages/' . $key . '.php', $args );
-        }
-        $pro_template = Utility::get_pro_template( 'settings/pages/' . $key . '.php', $args );
-
-        // If a valid template is returned, output it
-        if ( $template ) {
-            echo $template;
-        } else if ( $pro_template ) {
-            echo $pro_template;
-        } else {
-            ?>
-    <?php
-}
+        echo Utility::get_template( 'settings/pages/' . $key . '.php', $args );
     }
 }
 ?>
